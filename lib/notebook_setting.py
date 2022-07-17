a188507490a4d6f8fa52b0e3129b40c9fbdc92dc
Sourcery AI <>
2022-07-17 13:53:57 +0000

    'Refactored by Sourcery'

--- lib/notebook_setting.py
+++ lib/notebook_setting.py
@@ -17,17 +17,17 @@
 import sys, importlib
 
 def packageCheck(packageList):
-	print('[Python version] \t{}\n\n[Packages versions]\n'.format(sys.version))
+	print(f'[Python version] \t{sys.version}\n\n[Packages versions]\n')
 	for pack in packageList:
 		try:
 			try:
 				mod = importlib.import_module(pack)
 				print('{:20s}:\t{}'.format(pack,mod.__version__))
 			except:
-				mod = importlib.import_module(pack+".info")
+				mod = importlib.import_module(f"{pack}.info")
 				print('{:20s}:\t{}'.format(pack,mod.version))
 		except:
-			print("{} has to be installed".format(pack))
+			print(f"{pack} has to be installed")
 
 
 #####################################
@@ -51,23 +51,23 @@ from IPython import get_ipython
 from IPython.core.interactiveshell import InteractiveShell
 
 def find_notebook(fullname, path=None):
-    """find a notebook, given its fully qualified name and an optional path
+	"""find a notebook, given its fully qualified name and an optional path
     
     This turns "foo.bar" into "foo/bar.ipynb"
     and tries turning "Foo_Bar" into "Foo Bar" if Foo_Bar
     does not exist.
     """
-    name = fullname.rsplit('.', 1)[-1]
-    if not path:
-        path = ['']
-    for d in path:
-        nb_path = os.path.join(d, name + ".ipynb")
-        if os.path.isfile(nb_path):
-            return nb_path
-        # let import Notebook_Name find "Notebook Name.ipynb"
-        nb_path = nb_path.replace("_", " ")
-        if os.path.isfile(nb_path):
-            return nb_path
+	name = fullname.rsplit('.', 1)[-1]
+	if not path:
+	    path = ['']
+	for d in path:
+		nb_path = os.path.join(d, f"{name}.ipynb")
+		if os.path.isfile(nb_path):
+		    return nb_path
+		# let import Notebook_Name find "Notebook Name.ipynb"
+		nb_path = nb_path.replace("_", " ")
+		if os.path.isfile(nb_path):
+		    return nb_path
             
 
 class NotebookLoader(object):
@@ -77,39 +77,39 @@ class NotebookLoader(object):
         self.path = path
     
     def load_module(self, fullname):
-        """import a notebook as a module"""
-        path = find_notebook(fullname, self.path)
-        
-        print ("importing notebook from %s" % path)
-                                       
-        # load the notebook object
-        nb = nbformat.read(path, as_version=4)
-        
-        
-        # create the module and add it to sys.modules
-        # if name in sys.modules:
-        #    return sys.modules[name]
-        mod = types.ModuleType(fullname)
-        mod.__file__ = path
-        mod.__loader__ = self
-        mod.__dict__['get_ipython'] = get_ipython
-        sys.modules[fullname] = mod
-        
-        # extra work to ensure that magics that would affect the user_ns
-        # actually affect the notebook module's ns
-        save_user_ns = self.shell.user_ns
-        self.shell.user_ns = mod.__dict__
-        
-        try:
-          for cell in nb.cells:
-            if cell.cell_type == 'code':
-                # transform the input to executable Python
-                code = self.shell.input_transformer_manager.transform_cell(cell.source)
-                # run the code in themodule
-                exec(code, mod.__dict__)
-        finally:
-            self.shell.user_ns = save_user_ns
-        return mod
+    	"""import a notebook as a module"""
+    	path = find_notebook(fullname, self.path)
+
+    	print(f"importing notebook from {path}")
+
+    	# load the notebook object
+    	nb = nbformat.read(path, as_version=4)
+
+
+    	# create the module and add it to sys.modules
+    	# if name in sys.modules:
+    	#    return sys.modules[name]
+    	mod = types.ModuleType(fullname)
+    	mod.__file__ = path
+    	mod.__loader__ = self
+    	mod.__dict__['get_ipython'] = get_ipython
+    	sys.modules[fullname] = mod
+
+    	# extra work to ensure that magics that would affect the user_ns
+    	# actually affect the notebook module's ns
+    	save_user_ns = self.shell.user_ns
+    	self.shell.user_ns = mod.__dict__
+
+    	try:
+    	  for cell in nb.cells:
+    	    if cell.cell_type == 'code':
+    	        # transform the input to executable Python
+    	        code = self.shell.input_transformer_manager.transform_cell(cell.source)
+    	        # run the code in themodule
+    	        exec(code, mod.__dict__)
+    	finally:
+    	    self.shell.user_ns = save_user_ns
+    	return mod
 
 
 
